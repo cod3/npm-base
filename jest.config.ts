491dbbd087609c4cb5bc748028b37ab49d137daa
cod <cod@wild-eyes.sakura.ne.jp>
2020-12-27 22:50:54 +0900

    commonjsに巻き戻し

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,15 @@
-export default {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
     roots: ['<rootDir>/src'],
     testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    globals: {
+        'ts-jest': {
+            tsconfig: './tsconfig.json',
+        },
     },
 }
+export default config
